6dcaa682fa58494e81fb10f11d20907010ecea84
Muhammad Bilal <mb217029@gmail.com>
2026-04-14 23:20:05 +0500

    Week 1 Completed: Profile Picture Uploaded

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,19 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, TextInput, TouchableOpacity,
         StyleSheet, Alert, ActivityIndicator} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
+import {Image} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 
 function ProfileScreen({navigation}: any): React.JSX.Element {
-  // TASK 1: states for name, bio, loading
   const [name, setName] = useState('');
   const [bio, setBio] = useState('');
   const [loading, setLoading] = useState(true);
+  const [photoURL, setPhotoURL] = useState('');
 
   useEffect(() => {
-    // TASK 2: fetch user data from Firestore
-    // hint: firestore().collection('users').doc(uid).get()
-    // then set name and bio from the document data
     const fetchProfile = async() => {
         const uid = auth().currentUser?.uid;
         const doc = await firestore().collection('users').doc(uid).get();
@@ -21,17 +20,12 @@ function ProfileScreen({navigation}: any): React.JSX.Element {
         setName(data?.name);
         setBio(data?.bio);
         setLoading(false);
+        setPhotoURL(data?.photoURL || '');
     }
     fetchProfile();
   }, []);
 
   const handleUpdate = async () => {
-    // TASK 3: set loading true
-    // TASK 4: update the Firestore document with new name and bio
-    // hint: .update({ name, bio })
-    // TASK 5: Alert "Profile updated successfully"
-    // TASK 6: on error Alert the error
-    // TASK 7: finally set loading false
     setLoading(true);
 
     try{
@@ -45,15 +39,37 @@ function ProfileScreen({navigation}: any): React.JSX.Element {
     }
   };
 
+  const handlePickImage = async () => {
+    setLoading(true);
+  try{
+    const uid = auth().currentUser?.uid;
+    const response = await launchImageLibrary({mediaType: 'photo', includeBase64: true, quality: 0.5});
+    const asset = response.assets?.[0];
+    const base64String= `data:image/jpeg;base64,${asset?.base64}`;
+    setPhotoURL(base64String);
+    await firestore().collection('users').doc(uid).update({photoURL: base64String})
+  } catch (error: any){
+    Alert.alert("Profile Updation Failed", error.message)
+  } finally{
+    setLoading(false);
+  }
+};
+
   return (
-    // TASK 8: UI with:
-    // - Title "Edit Profile"
-    // - TextInput for name
-    // - TextInput for bio (multiline)
-    // - Save button
-    // - Logout button (reuse from HomeScreen)
     <View style={styles.container}>
         <Text style={styles.title}>Edit Profile</Text>
+
+        <TouchableOpacity onPress={handlePickImage}>
+          {photoURL ? (
+          <Image
+            source={{uri: photoURL}}
+            style={styles.avatar}/>
+          ) : (
+            <View style={styles.avatarPlaceholder}>
+              <Text>Tap to add photo</Text>
+            </View>
+          )}
+        </TouchableOpacity>
 
         <View style={styles.form}>
             <TextInput
@@ -97,63 +113,80 @@ function ProfileScreen({navigation}: any): React.JSX.Element {
 }
 
 const styles = StyleSheet.create ({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      paddingHorizontal: 24,
-      backgroundColor: '#fff',
-    },
-    title: {
-      fontSize: 32,
-      fontWeight: 'bold',
-      color: '#6C63FF',
-      textAlign: 'center',
-      marginBottom: 8,
-    },
-    subtitle: {
-      fontSize: 16,
-      color: '#888',
-      textAlign: 'center',
-      marginBottom: 32,
-    },
-    form :{
-      gap: 12
-    },
-    input :{
-      borderWidth: 1,
-      borderColor: '#ddd',
-      borderRadius: 10,
-      padding: 14,
-      fontSize: 16,
-      backgroundColor: '#f9f9f9',
-    },
-    error :{
-      color: 'red',
-      fontSize: 12,
-      marginTop: -8,
-    },
-    button: {
-      backgroundColor: '#6C63FF',
-      padding: 16,
-      borderRadius: 10,
-      alignItems: 'center',
-      marginTop: 8,
-    },
-    buttonText: {
-      color: '#fff',
-      fontSize: 16,
-      fontWeight: 'bold',
-    },
-    link: {
-      color: '#6C63FF',
-      textAlign: 'center',
-      marginTop: 16,
-    },
-    logoutLink: {
-      color: 'red',
-      textAlign: 'center',
-      marginTop: 8,
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 24,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#6C63FF',
+    textAlign: 'center',
+    marginBottom: 8,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#888',
+    textAlign: 'center',
+    marginBottom: 32,
+  },
+  form :{
+    gap: 12
+  },
+  input :{
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 10,
+    padding: 14,
+    fontSize: 16,
+    backgroundColor: '#f9f9f9',
+  },
+  error :{
+    color: 'red',
+    fontSize: 12,
+    marginTop: -8,
+  },
+  button: {
+    backgroundColor: '#6C63FF',
+    padding: 16,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 8,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  link: {
+    color: '#6C63FF',
+    textAlign: 'center',
+    marginTop: 16,
+  },
+  logoutLink: {
+    color: 'red',
+    textAlign: 'center',
+    marginTop: 8,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignSelf: 'center',
+    marginBottom: 16,
+  },
+  avatarPlaceholder: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: '#ddd',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
 })
 
 export default ProfileScreen;
